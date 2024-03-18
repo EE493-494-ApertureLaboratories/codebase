b131f69cd1b889217c24e1e0a798c8b6ada8199f
üsâme_aw <osamasaedawad@gmail.com>
2024-03-18 14:27:59 +0300

    added enables to joystick

--- mapping/src/joystick_controller.cpp
+++ mapping/src/joystick_controller.cpp
@@ -124,20 +124,15 @@ class JoystickDriver : public rclcpp::Node
     : Node("joystick_driver")
     {
       joy_subs_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "/joy", 10, std::bind(&JoystickDriver::command_motors, this, _1));
+      "/joy", 10, std::bind(&JoystickDriver::command_actuators, this, _1));
       
       communicator.init_comms("/dev/ttyACM0");
       //communicator.init_motors();
     }
 
   private:
-    void command_motors(const sensor_msgs::msg::Joy & msg) 
-    {
-      int forward_backward = (int)(msg.axes[1] * 65535);
-
-      int left = (int)  ( ((-msg.axes[4] + 1) / 2) * 65535);
-      int right = (int) ( ((-msg.axes[5] + 1) / 2) * 65535);
-      
+    
+    void command_motors(int forward_backward, int left, int right) {
       if(forward_backward > 500) {
         communicator.write_motors(1, forward_backward, 1, forward_backward);
       }
@@ -150,10 +145,36 @@ class JoystickDriver : public rclcpp::Node
       } else {
         communicator.write_motors(0, 0, 0, 0);
       }
+    }
 
+    void command_actuators(const sensor_msgs::msg::Joy & msg) 
+    {
+
+      int stop = msg.buttons[1];
+      int rotate = msg.buttons[3];
+      int servo_scan = msg.buttons[4];
+      int enable_motors = msg.buttons[5];
+
+      int forward_backward = (int)(msg.axes[1] * 65535);
+      int left = (int)  ( ((-msg.axes[4] + 1) / 2) * 65535);
+      int right = (int) ( ((-msg.axes[5] + 1) / 2) * 65535);
+      if(stop) {
+        communicator.stop_motors();
+      } else if(rotate) {
+        communicator.write_motors(1, 65535, 0, 65535);
+      } else if(servo_scan) {
+        communicator.servo(65535);
+      } else if(enable_motors) {
+        command_motors(forward_backward, left, right);
+      } else {
+        communicator.write_motors(0, 0, 0, 0);
+      }
     }
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subs_;
 };
+
+//      rclcpp::sleep_for(std::chrono::milliseconds(100));
+
 
 int main(int argc, char * argv[])
 {
