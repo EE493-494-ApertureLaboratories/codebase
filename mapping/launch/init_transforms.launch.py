e42842ec23ac3765d08379b12b8b27b28dbb9a9b
üsâme_aw <osamasaedawad@gmail.com>
2024-03-25 16:08:19 +0300

    added tf back for compatibility reasons

--- mapping/launch/init_transforms.launch.py
+++ mapping/launch/init_transforms.launch.py
@@ -28,13 +28,13 @@ def generate_launch_description():
                           '--frame-id', 'chassis_link', '--child-frame-id', 'left_wheel_link']
         ),
         ############################################
-        # Node(
-        #      package='tf2_ros',
-        #      executable='static_transform_publisher',
-        #      arguments = ['--x', '1.0', '--y', '-0.109.', '--z', '0.10', 
-        #                   '--yaw', '0.0', '--pitch', '0.0', '--roll', '0.0', 
-        #                   '--frame-id', 'chassis_link', '--child-frame-id', 'rotation_axis']
-        # ),
+        Node(
+             package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments = ['--x', '1.0', '--y', '-0.109.', '--z', '0.10', 
+                          '--yaw', '0.0', '--pitch', '0.0', '--roll', '0.0', 
+                          '--frame-id', 'chassis_link', '--child-frame-id', 'rotation_axis']
+        ),
 
         Node(
              package='tf2_ros',
