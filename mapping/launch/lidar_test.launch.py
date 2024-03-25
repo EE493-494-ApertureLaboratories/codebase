981ed2e8629aaaa86bc47dab67e352ce905560f7
üsâme_aw <osamasaedawad@gmail.com>
2024-03-25 14:37:40 +0300

    add ability to launch rviz2 thru launch argument

--- mapping/launch/lidar_test.launch.py
+++ mapping/launch/lidar_test.launch.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import LogInfo
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    
     channel_type =  LaunchConfiguration('channel_type', default='serial')
     serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
     serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
@@ -20,12 +21,16 @@ def generate_launch_description():
     scan_mode = LaunchConfiguration('scan_mode', default='Sensitivity')
 	
     rviz_config_dir = os.path.join(
-            get_package_share_directory('rplidar_ros'),
-            'rviz',
+            get_package_share_directory('mapping'),
+            'config',
             'rplidar_ros.rviz')
 
-
     return LaunchDescription([
+        
+        DeclareLaunchArgument(
+            'rviz',
+            default_value='false',
+            description='Display Scans in rviz2?'),
 
         DeclareLaunchArgument(
             'channel_type',
@@ -74,5 +79,15 @@ def generate_launch_description():
                            'scan_mode': scan_mode
                          }],
             output='screen'),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            #arguments=['-d', rviz_config_dir],
+            output='screen',
+            condition=IfCondition(LaunchConfiguration('rviz')),
+        ),
     ])
+
 
